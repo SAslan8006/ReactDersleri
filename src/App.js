8fcdadfec11a78883e8a98203cac3fbe8ca8abe3
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-02-20 15:58:55 +0300

    Prop Types

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import User from './componenets/User';
 
 const friends = [
-  { id: 1, name: "Ahmet" },
-  { id: 2, name: "Mehmet" },
-  { id: 3, name: "Zeynep" },
-  { id: 4, name: "Ayse" },
-  { id: 5, name: "Yasemin" }
+  { id: 1, name: "Ahmet", age: 23 },
+  { id: 2, name: "Mehmet", age: 29 },
+  { id: 3, name: "Zeynep", age: 32 },
+  { id: 4, name: "Ayse", age: 45 },
+  { id: 5, name: "Yasemin", age: 40 }
 ]
 function App() {
   return (
     < >
-      <User name="Mehmet" sname="Kara" isLoggedIn={true}
+      <User name="Mehmet" sname="Kara" age={23} isLoggedIn={true}
         friends={friends} />
     </>
   );
--- src/componenets/User.js
+++ src/componenets/User.js
@@ -1,10 +1,19 @@
-function User({ name, sname, isLoggedIn, friends }) {
+import ProTypes from "prop-types";
+
+function User({ name, sname, isLoggedIn, age, friends }) {
     return (
         <>
-            <div>{isLoggedIn ? `Selam ${name} ${sname}` : "Giriş Yapınız!"}</div>
+            <div>{isLoggedIn ? `Selam ${name} ${sname} ${age}` : "Giriş Yapınız!"}</div>
             {friends.map((friend, index) => <div key={friend.id}>{friend.name}</div>)}
         </>
     )
+}
+User.propTypes = {
+    name: ProTypes.string,
+    sname: ProTypes.string,
+    isLoggedIn: ProTypes.bool,
+    age: ProTypes.number,
+    friends: ProTypes.object
 }
 
 export default User;
