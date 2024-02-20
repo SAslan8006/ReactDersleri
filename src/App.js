5b904b8f224b1184d435f8f2c919cb03349136d3
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-02-20 10:54:32 +0300

    Koşullu Render İşlemi

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Header from './componenets/Header';
 const name = "Mahmut";
 const sname = "Kara"
+const isLoggedIn = false;
 function App() {
   return (
     < >
-      <h1>{`Benim Adım ${name} soyadım ${sname}`}</h1>
+      <h1>{isLoggedIn && `Benim Adım ${name} soyadım ${sname}`}</h1>
+      <h1>{!isLoggedIn && `Giriş Yapmadınız...`}</h1>
+
     </>
   );
 }
