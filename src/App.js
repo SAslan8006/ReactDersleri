c8eb78e7dd1b2316c3a2fac73e93963c494e2a49
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-02-20 10:36:39 +0300

    Jsx ve Temel Kurallar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
 import Header from './componenets/Header';
 
 function App() {
+  // return React.createElement("div", null, "Hello");
   return (
     <div >
       <Header />
+      <p className='xyz'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni perferendis repellendus facilis, reiciendis ad culpa assumenda excepturi iure ex officiis odio animi ipsum ab vitae cupiditate quaerat. Laborum, facilis eos?</p>
+      <label htmlFor="name">Name:
+        <input id='name' />
+      </label>
     </div>
   );
 }
