150cf4af481a30640d11135455616d95caffb735
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-02-21 08:58:38 +0300

    Array States

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 function App() {
   const [name, setName] = useState("Ahmet");
   const [age, setAge] = useState(20);
+  const [friends, setFriends] = useState(["Ahmet", "Mehmet", "Zeynep", "Ayse", "Yasemin"]);
   return (
     <div className='App' >
       <h1>Merhaba {name} Yaşım {age}</h1>
       <button onClick={() => setName("Mehmet")}>İsim Değiştir</button>
       <button onClick={() => setAge(25)}>Yaş Değiştir</button>
+      <hr />
+      <h1>Arkadaslar</h1>
+      {friends.map((friend, index) => <div key={index}>{friend}</div>)}
+      <button onClick={() => setFriends([...friends, "Ahmet"])}>Arkadaş Değiştir</button>
 
     </div>
   );
