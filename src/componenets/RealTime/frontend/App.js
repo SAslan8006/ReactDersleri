52fa027d350e164f692779a90b930945d3418beb
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-04 14:17:15 +0300

    Chat App Giriş

---
+++ src/componenets/RealTime/frontend/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Pallet from './componenets/RealTime/frontend/Pallet';
+import { init, subscribe } from './componenets/RealTime/frontend/socketApi';
+
+function App() {
+    const [activeColor, setColor] = useState("#000000");
+    useEffect(() => {
+        init();
+        subscribe((color) => {
+            setColor(color);
+        });
+    }, []);
+    return (
+        <div className='App' style={{ backgroundColor: activeColor }}>
+            <h1>{activeColor}</h1>
+            <Pallet activeColor={activeColor} />
+        </div>
+    )
+}
+
+export default App;
