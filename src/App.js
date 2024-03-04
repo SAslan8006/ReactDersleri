135429d84ad30b71a50836bd9e96ed8085d90782
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-04 15:51:43 +0300

    Chat Context

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import Pallet from './componenets/RealTime/frontend/Pallet';
-import { init, subscribe } from './componenets/RealTime/frontend/socketApi';
+import { ChatProvider } from './componenets/ChapApp/Frondend/context/ChatContext';
+import Container from './componenets/ChapApp/Frondend/Container';
 
 function App() {
-  const [activeColor, setColor] = useState("#000000");
-  useEffect(() => {
-    init();
-    subscribe((color) => {
-      setColor(color);
-    });
-  }, []);
+
   return (
-    <div className='App' style={{ backgroundColor: activeColor }}>
-      <h1>{activeColor}</h1>
-      <Pallet activeColor={activeColor} />
-    </div>
+    <ChatProvider>
+      <Container />
+    </ChatProvider>
   )
 }
 
---
+++ src/componenets/ChapApp/Frondend/context/ChatContext.js
@@ -0,0 +1,14 @@
+const { createContext, useState } = require("react");
+
+const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+    const [message, setMessage] = useState([]);
+    const values = {
+        message,
+        setMessage,
+    }
+    return <ChatContext.Provider value={values}>{children}</ChatContext.Provider>
+}
+
+export default ChatContext;
