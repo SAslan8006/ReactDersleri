ae3c61b71de1c39897e2226ccd2ce0072cbdd40f
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-03-06 09:17:39 +0300

    İntl kurmak , Default Locale  ve Parametre Geçmek

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import './App.css';
-import { ChatProvider } from './componenets/ChapApp/Frondend/context/ChatContext';
-import Container from './componenets/ChapApp/Frondend/Container';
+import "./App.css";
 
+import { useState, useEffect } from "react";
+import { IntlProvider, FormattedMessage } from "react-intl";
+const messages = {
+  "tr-TR": {
+    title: "Merhaba Dünya",
+    description: "{count} yeni mesajınız var",
+  },
+  "en-US": {
+    title: "Hello World",
+    description: "You have {count} new messages",
+  },
+};
 
 function App() {
+  const isLocale = localStorage.getItem("locale");
+  const defaultLocale = isLocale ? isLocale : navigator.language;
+  const [locale, setLocale] = useState(defaultLocale);
+  console.log(navigator.language);
+  useEffect(() => {
+    localStorage.setItem("locale", locale);
+  }, [locale]);
+
   return (
-    <ChatProvider>
-      <Container />
-    </ChatProvider>
+    <div className="App">
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <FormattedMessage id="title" />
+
+        <p>
+          <FormattedMessage id="description" values={{ count: 5 }} />
+        </p>
+
+        <br />
+        <br />
+        <button onClick={() => setLocale("tr-TR")}>TR</button>
+        <button onClick={() => setLocale("en-US")}>EN</button>
+      </IntlProvider>
+    </div>
   );
 }
 
